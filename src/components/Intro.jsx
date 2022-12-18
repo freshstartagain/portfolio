38e9120a98a160cf0c1cccfec7ef3b131833ddfb
freshstartagain <newstartagainhays@gmail.com>
2022-12-19 00:59:59 +0800

    feat: format intro

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -2,98 +2,22 @@ import React from "react";
 
 function Intro() {
   return (
-    <div className="flex flex-col items-center justify-center text-center pb-6 pt-20">
-      <h1 className="font-bold text-4xl mb-1">Lester</h1>
-      <p className="font-medium text-base mb-3 ">Software & Data Engineer</p>
-      <p className="font-bold mb-6 text-sm">
+    <div className="flex flex-col items-center justify-center pb-6 pt-20 text-center">
+      <h1 className="dark:text-white font-bold mb-1 text-7xl">Lester</h1>
+      <p className="font-medium text-base mb-3 text-xl">
+        Software & Data Engineer
+      </p>
+      <p className="font-bold max-w-xl text-md">
         Hi, my name is Lester and I am a software and data engineer with a
         diverse background in different industries. I have spent most of my
-        career working in the government, bank, startup, and legal analytics sectors,
-        where I have developed and maintained software systems and delivered
-        data solutions using a wide range of technologies. I have a unique
-        ability to switch between software and data engineering, which allows me
-        to tackle complex challenges and create innovative solutions. With my
-        extensive experience and technical skills, I am confident that I can
-        contribute to any team and help drive successful projects.
+        career working in the government, bank, startup, and legal analytics
+        sectors, where I have developed and maintained software systems and
+        delivered data solutions using a wide range of technologies.
+        <br /><br />I have a unique ability to switch between software and data
+        engineering, which allows me to tackle complex challenges and create
+        innovative solutions. I am confident that I can contribute to any team
+        and help drive successful projects.
       </p>
-      <p className="font-bold mb-6 text-sm">
-        Hi, my name is Lester and I am a software and data engineer with a
-        diverse background in different industries. I have spent most of my
-        career working in the government, bank, startup, and legal analytics sectors,
-        where I have developed and maintained software systems and delivered
-        data solutions using a wide range of technologies. I have a unique
-        ability to switch between software and data engineering, which allows me
-        to tackle complex challenges and create innovative solutions. With my
-        extensive experience and technical skills, I am confident that I can
-        contribute to any team and help drive successful projects.
-      </p>
-      <p className="font-bold mb-6 text-sm">
-        Hi, my name is Lester and I am a software and data engineer with a
-        diverse background in different industries. I have spent most of my
-        career working in the government, bank, startup, and legal analytics sectors,
-        where I have developed and maintained software systems and delivered
-        data solutions using a wide range of technologies. I have a unique
-        ability to switch between software and data engineering, which allows me
-        to tackle complex challenges and create innovative solutions. With my
-        extensive experience and technical skills, I am confident that I can
-        contribute to any team and help drive successful projects.
-      </p>
-      <p className="font-bold mb-6 text-sm">
-        Hi, my name is Lester and I am a software and data engineer with a
-        diverse background in different industries. I have spent most of my
-        career working in the government, bank, startup, and legal analytics sectors,
-        where I have developed and maintained software systems and delivered
-        data solutions using a wide range of technologies. I have a unique
-        ability to switch between software and data engineering, which allows me
-        to tackle complex challenges and create innovative solutions. With my
-        extensive experience and technical skills, I am confident that I can
-        contribute to any team and help drive successful projects.
-      </p>
-      <p className="font-bold mb-6 text-sm">
-        Hi, my name is Lester and I am a software and data engineer with a
-        diverse background in different industries. I have spent most of my
-        career working in the government, bank, startup, and legal analytics sectors,
-        where I have developed and maintained software systems and delivered
-        data solutions using a wide range of technologies. I have a unique
-        ability to switch between software and data engineering, which allows me
-        to tackle complex challenges and create innovative solutions. With my
-        extensive experience and technical skills, I am confident that I can
-        contribute to any team and help drive successful projects.
-      </p>
-      <p className="font-bold mb-6 text-sm">
-        Hi, my name is Lester and I am a software and data engineer with a
-        diverse background in different industries. I have spent most of my
-        career working in the government, bank, startup, and legal analytics sectors,
-        where I have developed and maintained software systems and delivered
-        data solutions using a wide range of technologies. I have a unique
-        ability to switch between software and data engineering, which allows me
-        to tackle complex challenges and create innovative solutions. With my
-        extensive experience and technical skills, I am confident that I can
-        contribute to any team and help drive successful projects.
-      </p>
-      <p className="font-bold mb-6 text-sm">
-        Hi, my name is Lester and I am a software and data engineer with a
-        diverse background in different industries. I have spent most of my
-        career working in the government, bank, startup, and legal analytics sectors,
-        where I have developed and maintained software systems and delivered
-        data solutions using a wide range of technologies. I have a unique
-        ability to switch between software and data engineering, which allows me
-        to tackle complex challenges and create innovative solutions. With my
-        extensive experience and technical skills, I am confident that I can
-        contribute to any team and help drive successful projects.
-      </p>
-      <p className="font-bold mb-6 text-sm">
-        Hi, my name is Lester and I am a software and data engineer with a
-        diverse background in different industries. I have spent most of my
-        career working in the government, bank, startup, and legal analytics sectors,
-        where I have developed and maintained software systems and delivered
-        data solutions using a wide range of technologies. I have a unique
-        ability to switch between software and data engineering, which allows me
-        to tackle complex challenges and create innovative solutions. With my
-        extensive experience and technical skills, I am confident that I can
-        contribute to any team and help drive successful projects.
-      </p>
-      
     </div>
   );
 }
