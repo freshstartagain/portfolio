4791fa60d013e7e4d73736651b18069150af872b
freshstartagain <newstartagainhays@gmail.com>
2022-12-19 20:07:41 +0800

    feat: timeline data

---
+++ src/data/timeline.js
@@ -0,0 +1,34 @@
+export default [
+  {
+    year: "2020",
+    title: "Data Engineer",
+    company: "goPassport",
+    duration: "1 year 1 month",
+    details:
+      "Determining data storage needs and building a data warehouse for internal departments, as well as creating and enhancing data solutions to enable seamless data delivery. Developing complex applications to extract, transform, and load data, and creating API endpoints using Azure technologies",
+  },
+  {
+    year: "2019",
+    title: "Junior Data Engineer",
+    company: "Pynotech Solutions Inc.",
+    duration: "6 months",
+    details:
+      "Designing, maintaining, and optimizing a stable and reliable database system. Creating ETL/ELT pipelines to facilitate data management and support users in their data-related needs.",
+  },
+  {
+    year: "2019",
+    title: "IT Assistant",
+    company: "Rural Bank of Pilar",
+    duration: "5 months",
+    details:
+      "Maintaining and managing the bank's information technology systems and networks, including installing and configuring computer systems, diagnosing hardware and software faults, solving technical and application problems, and recommending purchases of hardware and software.",
+  },
+  {
+    year: "2018",
+    title: "IT Staff",
+    company: "LGU Pilar Sorsogon",
+    duration: "6 months",
+    details:
+      "Maintaining a WordPress website, ensuring that it is up to date, secure, and functioning properly. Creating new pages or blog posts, uploading images or videos, and organizing the content.",
+  },
+];
