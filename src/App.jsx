6a8fc7d937ebdf058b84698c6b8ecea3cc1dbdb0
freshstartagain <newstartagainhays@gmail.com>
2022-12-24 03:14:57 +0800

    fix: sm nav view

--- src/App.jsx
+++ src/App.jsx
@@ -8,8 +8,8 @@ import Footer from "./components/Footer";
 function App() {
   return (
     <div className="min-h-screen bg-white font-inter text-stone-900 dark:bg-stone-900 dark:text-stone-300">
+      <Navigation />
       <div className="mx-auto w-11/12 max-w-5xl dark:bg-stone-900">
-        <Navigation />
         <Intro />
         <Timeline />
         <Tools />
