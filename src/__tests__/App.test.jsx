eb6a7b52aef7f1b2ad7d5fecb2c03b473b3a3e58
freshstartagain <newstartagainhays@gmail.com>
2022-12-31 00:05:13 +0800

    feat: initial test

--- src/__tests__/App.test.jsx
+++ src/__tests__/App.test.jsx
@@ -1,8 +1,44 @@
+import React from 'react';
 import './matchMedia.mock';
-import { render } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import timeline from "../data/timeline";
 import App from "../App";
 
 
-test('renders the landing page', () => {
+test('renders the app component', () => {
     render(<App />);
+
+    // Test navigation section
+    const navText = ['Profile', 'Timeline', 'Tools']
+
+    navText.map((text) => {
+        let link = screen.getAllByText(text)[0]
+        userEvent.click(link);
+        expect(link.href).toContain(`#${text.toLowerCase()}`);
+    });
+
+    // Test all heading
+    const headings = ['Lester', 'Timeline', 'Tools'];
+
+    headings.map((heading) => {
+        expect(screen.getByRole('heading', { name: heading }));
+    });
+
+    // Test timeline
+    timeline.map((x) => {
+        expect(screen.getAllByText(x.year)[0]);
+        expect(screen.getAllByText(x.company)[0]);
+        expect(screen.getAllByText(x.title)[0]);
+        expect(screen.getAllByText(x.duration)[0]);
+        expect(screen.getAllByText(x.details)[0]);
+    });
+
+    // Test tools
+    const tools = ['Frontend', 'Backend', 'Dev Ops', 'Project Management'];
+
+    tools.map((tool) => {
+        expect(screen.getAllByText(tool));
+    });
 });
