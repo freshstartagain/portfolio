05156239a852617eea1fc6171bc4446aeb2d7bc2
freshstartagain <newstartagainhays@gmail.com>
2022-12-31 00:06:52 +0800

    feat: update prettier scope and format test

--- src/__tests__/App.test.jsx
+++ src/__tests__/App.test.jsx
@@ -1,44 +1,43 @@
-import React from 'react';
-import './matchMedia.mock';
-import '@testing-library/jest-dom';
-import { render, screen } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
+import React from "react";
+import "./matchMedia.mock";
+import "@testing-library/jest-dom";
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import timeline from "../data/timeline";
 import App from "../App";
 
+test("renders the app component", () => {
+  render(<App />);
 
-test('renders the app component', () => {
-    render(<App />);
+  // Test navigation section
+  const navText = ["Profile", "Timeline", "Tools"];
 
-    // Test navigation section
-    const navText = ['Profile', 'Timeline', 'Tools']
+  navText.map((text) => {
+    let link = screen.getAllByText(text)[0];
+    userEvent.click(link);
+    expect(link.href).toContain(`#${text.toLowerCase()}`);
+  });
 
-    navText.map((text) => {
-        let link = screen.getAllByText(text)[0]
-        userEvent.click(link);
-        expect(link.href).toContain(`#${text.toLowerCase()}`);
-    });
+  // Test all heading
+  const headings = ["Lester", "Timeline", "Tools"];
 
-    // Test all heading
-    const headings = ['Lester', 'Timeline', 'Tools'];
+  headings.map((heading) => {
+    expect(screen.getByRole("heading", { name: heading }));
+  });
 
-    headings.map((heading) => {
-        expect(screen.getByRole('heading', { name: heading }));
-    });
+  // Test timeline
+  timeline.map((x) => {
+    expect(screen.getAllByText(x.year)[0]);
+    expect(screen.getAllByText(x.company)[0]);
+    expect(screen.getAllByText(x.title)[0]);
+    expect(screen.getAllByText(x.duration)[0]);
+    expect(screen.getAllByText(x.details)[0]);
+  });
 
-    // Test timeline
-    timeline.map((x) => {
-        expect(screen.getAllByText(x.year)[0]);
-        expect(screen.getAllByText(x.company)[0]);
-        expect(screen.getAllByText(x.title)[0]);
-        expect(screen.getAllByText(x.duration)[0]);
-        expect(screen.getAllByText(x.details)[0]);
-    });
+  // Test tools
+  const tools = ["Frontend", "Backend", "Dev Ops", "Project Management"];
 
-    // Test tools
-    const tools = ['Frontend', 'Backend', 'Dev Ops', 'Project Management'];
-
-    tools.map((tool) => {
-        expect(screen.getAllByText(tool));
-    });
+  tools.map((tool) => {
+    expect(screen.getAllByText(tool));
+  });
 });
